71fae65bc6fee49bc6254b95842bcd9b612cdae2
mstsurnalyakter <surnalyms@gmail.com>
2024-04-26 09:38:32 +0600

    Basic layout and structure

---
+++ src/layouts/Root.jsx
@@ -0,0 +1,17 @@
+import { Outlet } from "react-router-dom";
+import Footer from "../pages/Shared/Footer/Footer";
+import NavBar from "../pages/Shared/NavBar/NavBar";
+
+const Root = () => {
+  return (
+    <div className="font-poppins">
+      <NavBar />
+      <div className="mx-auto max-w-7xl px-4 lg:px-2  mt-10 mb-10 min-h-[calc(100vh-432px)]">
+        <Outlet />
+      </div>
+      <Footer />
+    </div>
+  );
+};
+
+export default Root;
