5c51dff8d5840aecf5742abd95198761a67cc2bb
mstsurnalyakter <surnalyms@gmail.com>
2024-04-28 19:44:06 +0600

    react-simple-typewriter package install

--- src/components/ContactUs/ContactUs.jsx
+++ src/components/ContactUs/ContactUs.jsx
@@ -1,10 +1,17 @@
-
+import { useTypewriter, Cursor } from "react-simple-typewriter";
 
 const ContactUs = () => {
+
+    const [text] = useTypewriter({
+      words: ["Contact Us", "For any support just send your query"],
+      loop: 0,
+    });
+
   return (
     <div className="shadow-lg mt-20 border rounded-l-xl px-6 items-center min-h-[500px] ">
       <h3 className="w-2/3 mx-auto text-center mt-10 font-extrabold text-2xl">
-        For any support just send your query
+        <span>{text}</span>
+        <Cursor />
       </h3>
       <div className="grid grid-cols-1 lg:grid-cols-2  items-center gap-6 ">
         <div>
@@ -40,6 +47,6 @@ const ContactUs = () => {
       </div>
     </div>
   );
-}
+};
 
-export default ContactUs
+export default ContactUs;
