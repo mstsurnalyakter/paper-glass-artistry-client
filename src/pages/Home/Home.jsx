075714cfabb4223e68b435848e270e3350cff964
mstsurnalyakter <surnalyms@gmail.com>
2024-04-28 18:59:24 +0600

    ContactUs section add

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,4 +1,5 @@
 import Banner from "../../components/Banner/Banner";
+import ContactUs from "../../components/ContactUs/ContactUs";
 import CraftItems from "../../components/CraftItems/CraftItems";
 import DynamicTitle from "../../components/DynamicTitle/DynamicTitle";
 import Subcategories from "../../components/Subcategories/Subcategories";
@@ -14,6 +15,7 @@ const Home = () => {
         <CraftItems/>
         <Subcategories/>
         <Testimonials/>
+        <ContactUs/>
       </div>
     </div>
   );
