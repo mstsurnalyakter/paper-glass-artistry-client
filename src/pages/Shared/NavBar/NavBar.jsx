d0f080609e4cb0778d6351621795d9d5705fa0d8
mstsurnalyakter <surnalyms@gmail.com>
2024-04-29 08:54:28 +0600

    NavBar section responsive style add

--- src/pages/Shared/NavBar/NavBar.jsx
+++ src/pages/Shared/NavBar/NavBar.jsx
@@ -181,7 +181,7 @@ const NavBar = () => {
               )}
             </div>
           </div>
-          <div className="flex gap-4">
+          <div className="flex flex-col sm:flex-row gap-4">
             <label className="cursor-pointer grid place-items-center">
               <input
                 type="checkbox"
